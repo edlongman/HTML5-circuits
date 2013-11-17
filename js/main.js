1297e32d7837da19ba4ec1263c3c5ed7a6c6929d
Edward Longman <eialongman@gmail.com>
2013-11-17 17:14:03 +0000

    basics of the objects on which the application will be built

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//One object to rule them all
+(function(window, undefined) {
+	Board={};
+	Board.component=function(){}
+	Board.components={};
+	Board.circuit={};//holds positions of components
+	Board.circuit.prototype.Draw=function(){};
+	componentProperties={
+		symbol:"path/to/img.png",
+		name:"name",
+		components:new circuit(),
+		output:function(){
+			
+		}
+	}
+	Board.component.prototype.Draw=function(){};
+})(window);
