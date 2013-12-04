0dedc3d0c5c19627f59e079428561dbbb9147701
Edward Longman <eialongman@gmail.com>
2013-12-04 18:56:28 +0000

    add hack for inserting path SVG elements

--- js/main.js
+++ js/main.js
@@ -1,100 +1,3 @@
-/**
- * innerHTML property for SVGElement
- * Copyright(c) 2010, Jeff Schiller
- *
- * Licensed under the Apache License, Version 2
- *
- * Works in a SVG document in Chrome 6+, Safari 5+, Firefox 4+ and IE9+.
- * Works in a HTML5 document in Chrome 7+, Firefox 4+ and IE9+.
- * Does not work in Opera since it doesn't support the SVGElement interface yet.
- *
- * I haven't decided on the best name for this property - thus the duplication.
- */
-
-(function() {
-var serializeXML = function(node, output) {
-  var nodeType = node.nodeType;
-  if (nodeType == 3) { // TEXT nodes.
-    // Replace special XML characters with their entities.
-    output.push(node.textContent.replace(/&/, '&amp;').replace(/</, '&lt;').replace('>', '&gt;'));
-  } else if (nodeType == 1) { // ELEMENT nodes.
-    // Serialize Element nodes.
-    output.push('<', node.tagName);
-    if (node.hasAttributes()) {
-      var attrMap = node.attributes;
-      for (var i = 0, len = attrMap.length; i < len; ++i) {
-        var attrNode = attrMap.item(i);
-        output.push(' ', attrNode.name, '=\'', attrNode.value, '\'');
-      }
-    }
-    if (node.hasChildNodes()) {
-      output.push('>');
-      var childNodes = node.childNodes;
-      for (var i = 0, len = childNodes.length; i < len; ++i) {
-        serializeXML(childNodes.item(i), output);
-      }
-      output.push('</', node.tagName, '>');
-    } else {
-      output.push('/>');
-    }
-  } else if (nodeType == 8) {
-    // TODO(codedread): Replace special characters with XML entities?
-    output.push('<!--', node.nodeValue, '-->');
-  } else {
-    // TODO: Handle CDATA nodes.
-    // TODO: Handle ENTITY nodes.
-    // TODO: Handle DOCUMENT nodes.
-    throw 'Error serializing XML. Unhandled node of type: ' + nodeType;
-  }
-}
-// The innerHTML DOM property for SVGElement.
-Object.defineProperty(SVGElement.prototype, 'innerHTML', {
-  get: function() {
-    var output = [];
-    var childNode = this.firstChild;
-    while (childNode) {
-      serializeXML(childNode, output);
-      childNode = childNode.nextSibling;
-    }
-    return output.join('');
-  },
-  set: function(markupText) {
-    // Wipe out the current contents of the element.
-    while (this.firstChild) {
-      this.removeChild(this.firstChild);
-    }
-
-    try {
-      // Parse the markup into valid nodes.
-      var dXML = new DOMParser();
-      dXML.async = false;
-      // Wrap the markup into a SVG node to ensure parsing works.
-      sXML = '<svg xmlns=\'http://www.w3.org/2000/svg\'>' + markupText + '</svg>';
-      var svgDocElement = dXML.parseFromString(sXML, 'text/xml').documentElement;
-
-      // Now take each node, import it and append to this element.
-      var childNode = svgDocElement.firstChild;
-      while(childNode) {
-        this.appendChild(this.ownerDocument.importNode(childNode, true));
-        childNode = childNode.nextSibling;
-      }
-    } catch(e) {
-      throw new Error('Error parsing XML string');
-    };
-  }
-});
-
-// The innerSVG DOM property for SVGElement.
-Object.defineProperty(SVGElement.prototype, 'innerSVG', {
-  get: function() {
-    return this.innerHTML;
-  },
-  set: function(markupText) {
-    this.innerHTML = markupText;
-  }
-});
-
-})();
 //One object to rule them all
 (function(window, undefined) {
 	Board={};
@@ -128,6 +31,7 @@ Object.defineProperty(SVGElement.prototype, 'innerSVG', {
 	Board.circuit.prototype.Draw=function(domElement){
 		var components=$(domElement).find("div");
 		var lines=$(domElement).find("svg");
+		var svgns=lines[0].xmlns;
 		components.html("");
 		lines.html("");
 		for(var i=0;i<this.parts.length;i++){
@@ -138,19 +42,27 @@ Object.defineProperty(SVGElement.prototype, 'innerSVG', {
 			});
 			components.append(component);
 			for(var ii=0;ii<componentObj.inputs.length;ii++){
-				//draw line from this to other component
-				var fromX=componentObj.inputs[ii].parent.x,
-					fromY=componentObj.inputs[ii].parent.y,
-					toX=componentObj.x,
-					toY=componentObj.y;
-				var control1=fromX+minimum(40,Math.floor((fromY-fromX)/3));
-				var control2=toX-minimum(40,Math.floor((fromY-fromX)/3));
-				var lineArg="M "+fromX+" "+fromY+" C "+control1+" "+fromY+" ";
-					lineArg+=control2+" "+toY+" "+toX+" "+toY;
-				line=$("<path class='connector' d='"+lineArg+"'>");
-				lines.append(line);
+				if(componentObj.inputs[ii].pair!=undefined&&componentObj.inputs[ii].pair.parent!=undefined){
+					//draw line from this to other component
+					var fromX=componentObj.inputs[ii].pair.parent.x,
+						fromY=componentObj.inputs[ii].pair.parent.y,
+						toX=componentObj.x,
+						toY=componentObj.y;
+					var control1=fromX+minimum(40,Math.floor((toX-fromX)/3));
+					var control2=toX-minimum(40,Math.floor((toX-fromX)/3));
+					var lineArg="M"+fromX+","+fromY+" C"+control1+","+fromY+" ";
+						lineArg+=control2+","+toY+" "+toX+","+toY;
+					//this basically adds a line but jQuery doesn't work well with SVG so is a bit of a hack
+					//http://stackoverflow.com/questions/6698480/
+					line=document.createElementNS(svgns,"path");
+					line.setAttributeNS(null,"class","connector");
+					line.setAttributeNS(null,"d",lineArg);
+					lines[0].appendChild(line);
+				}
 			}
 		}
+		//this hack is to fix the fact that for some reason unbeknown to me currently it wouldn't show
+		lines[0].innerHTML=lines.html();
 	};
 	componentProperties={
 		symbol:"path/to/img.png",
